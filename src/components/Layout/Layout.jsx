f9ae32188d85f401d2b32be05f2b6ad7500a5ba1
Tawfykelgohre888 <tawfykelgohre8@gmail.com>
2025-02-04 15:31:37 +0200

    second

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import styles from './Layout.module.css'
-import Navbar from '../Navbar/Navbar.Jsx'
+
 import { Outlet } from 'react-router-dom'
 import Footer from '../Footer/Footer'
+import Navbar from '../Navbar/Navbar';
 export default function Layout() {
   return (
-   <div className= 'd-flex flex-column justify-content-between min-vh-100'>
-    <Navbar/>
-   <div className={styles.paddingSection}>
-   <Outlet/>
-   </div>
-    <Footer/>
-   </div>
-  )
+    <div className="d-flex flex-column justify-content-between min-vh-100">
+      <Navbar/>
+      <div className={styles.paddingSection}>
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
 }
